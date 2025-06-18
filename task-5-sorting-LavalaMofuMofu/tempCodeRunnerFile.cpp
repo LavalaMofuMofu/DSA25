f328195982502349f5c4afe9ee9d1a0120de85b3
Muhammad Irgi Fahrezha <2410817210005@mhs.ulm.ac.id>
2025-06-18 22:43:33 +0800

    Delete task-5-sorting-LavalaMofuMofu/tempCodeRunnerFile.cpp

--- task-5-sorting-LavalaMofuMofu/tempCodeRunnerFile.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include <random>
-
-int main() {
-    std::mt19937_64 rng(time(0)); // 12345 adalah seed
-    std::cout << rng() << std::endl; // menghasilkan angka acak 64-bit
-    return 0;
-}
